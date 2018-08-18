6548c73999a5061f73507d723998cdc9cbdee029
heatgher <1426855169@qq.com>
2018-08-18 18:33:59 +0800

    新增实体类 Car

--- carssm/src/main/java/com/ssm/pojo/Car.java
+++ carssm/src/main/java/com/ssm/pojo/Car.java
@@ -4,4 +4,22 @@ package com.ssm.pojo;
  * 新建pojo类
  */
 public class Car {
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
